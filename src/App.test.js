70dd89601bb66f415e5aa73e8ab5015a6f13d17c
João Vitor F <joaovitor.jobs2001@gmail.com>
2022-06-28 17:00:41 -0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react"
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it("should be able to render the component correctly", () => {
+    render(<App />)
+  
+    const textElement = screen.queryByText('Hello,')
+    expect(textElement).toBeInTheDocument()
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    expect(inputElement).toBeInTheDocument()
+  
+    const imageElement = screen.queryByRole('img')
+    expect(imageElement).toBeInTheDocument()
+  })
+  
+  it('should be able to find the imagem by the correct alt text', () => {
+    render(<App />)
+  
+    const altTextImageElement = screen.getByAltText('ilustração de uma mulher negra usando o computador e segurando uma xícara')
+    expect(altTextImageElement).toBeInTheDocument()
+  })
+  
+  it('should be able to get input value correctly', () => {
+    render(<App />)
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'João'
+      }
+    })
+  
+    const typedValue = screen.queryByText('João')
+  
+    expect(typedValue).toHaveTextContent('João')
+  })
+  
+  it('should be able to render the correct content after user type', () => {
+    render(<App />)
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome')
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'João'
+      }
+    })
+  
+    expect(screen.queryByText('João')).toHaveTextContent('João')
+    expect(screen.queryByText('Hello,')).toHaveTextContent('Hello, João')
+  })
+})
